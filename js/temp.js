4b06aeb3f7983eb4fa3dc87dd889ad99311bf94e
Fanghan Hu <Fanghan.Hu@gmail.com>
2019-01-18 01:00:39 -0600

    styles for cards and selection/display function for showing selected cards.

--- js/temp.js
+++ js/temp.js
@@ -1,6 +1,7 @@
 //////////////////////////////////////////////////
 $(function () {
     loadData();
+    init();
 
 });
 //////////////////////////////////////////////////
@@ -21,9 +22,19 @@ var SpecialEnum = {
         4: "#929175",
         5: "#C18768",
         6: "#9390A4"
+    },
+    name: {
+        0: "strength",
+        1: "perception",
+        2: "endurance",
+        3: "charisma",
+        4: "intelligence",
+        5: "agility",
+        6: "luck"
     }
-}
+};
 
+//used for wrapping selection data into url
 var dictionary = "qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlLzZxXcCvVbBnNmM1234567890";
 
 //Stores cards that haven't been selected
@@ -31,6 +42,12 @@ var selectable = [[],[],[],[],[],[],[]];
 
 //Stores cards that have been selected.
 var selected = [[],[],[],[],[],[],[]];
+
+//the special stats, each selected card cost
+var special = [1, 1, 1, 1, 1, 1, 1];
+
+//the perk points left, selecting a perk card cost [the cost amount + card level - 1] points.
+var points = 49;
 
 /**
  * loads card data from json file.
@@ -48,10 +65,37 @@ function loadData() {
                 var perk = data[i][j];
                 //represent which special stat this perk card belongs.
                 perk.special = i;
+                perk.level = 1;
                 selectable[i].push(perk);
             }
+            showSelectableCards(SpecialEnum.STRENGTH);
         }
     })
+}
+
+/**
+ * setup events
+ */
+function init() {
+
+    /**
+     * show selectable cards when clicking a stats card.
+     */
+    $("#stats-section").delegate(".stats-card", "click", function () {
+        showSelectableCards($(this).index());
+    })
+
+
+}
+
+function showSelectableCards(special) {
+    var $cardSelection = $("#card-selection");
+    $cardSelection.empty();
+    for(var i = 0; i < selectable[special].length; i++) {
+        var card = createCardElement(selectable[special][i]);
+        $cardSelection.append(card);
+        $(card).hide().fadeIn(Math.floor(Math.random() * 600));
+    }
 }
 
 /**
@@ -63,4 +107,118 @@ function copyLink() {
     copyText.select();
     document.execCommand("copy");
     alert("已复制包含加点内容的连接:\n" + copyText.value);
+}
+
+/**
+ * returns the actual amount of cost of a given cost
+ * @param cardData
+ */
+function getCost(cardData) {
+    if(cardData.hasOwnProperty("initialCost"))
+    {
+        if(cardData.hasOwnProperty("level"))
+        {
+            return cardData.initialCost + cardData.level - 1;
+        }
+
+        return cardData.initialCost;
+    }
+
+    return undefined;
+}
+
+function getDescription(cardData) {
+    if(cardData.hasOwnProperty("desc") && cardData.hasOwnProperty("level")) {
+        return cardData.desc[cardData.level-1];
+    }
+
+    return "error reading card description."
+}
+
+/**
+ * Create an card element using the given card data
+ * @param cardData
+ * @param isSelected true if the created element should represent an selected card.
+ */
+function createCardElement(cardData, isSelected) {
+    var card = document.createElement("div");
+    card.className = "perk-card";
+    if(cardData.hasOwnProperty("special")) {
+        card.className += " " + SpecialEnum.name[cardData.special];
+    }
+
+    var cardHeader = document.createElement("div");
+    cardHeader.className = "perk-card-header";
+    card.appendChild(cardHeader);
+
+    var cardCost = document.createElement("span");
+    cardCost.className = "perk-card-cost";
+    cardCost.textContent = getCost(cardData);
+    cardHeader.appendChild(cardCost);
+
+
+    var cardName = document.createElement("span");
+    cardName.className = "perk-card-name";
+    cardName.textContent = cardData.name;
+    cardHeader.appendChild(cardName);
+
+    var cardBody = document.createElement("div");
+    cardBody.className = "perk-card-body";
+    card.appendChild(cardBody);
+
+    var desc = document.createElement("p");
+    desc.textContent = getDescription(cardData);
+    cardBody.appendChild(desc);
+
+    var cardFooter = document.createElement("div");
+    cardFooter.className = "perk-card-footer";
+    card.appendChild(cardFooter);
+
+    if(cardData.hasOwnProperty("desc") && cardData.hasOwnProperty("level")) {
+        for(var i = 0; i < cardData.desc.length; i++)
+        {
+            var star = document.createElement("i");
+            star.className = "fas fa-star star";
+            if(i < cardData.level)
+            {
+                star.className += " active-star";
+            }
+            cardFooter.appendChild(star);
+        }
+    }
+
+    return card;
+}
+
+/**
+ * move a card from selectable to selected, point calculation should be done here
+ * @param card
+ */
+function selectCard(card) {
+
+}
+
+function deselectCard(card) {
+
+}
+
+/**
+ * update
+ */
+function updateBuild() {
+
+}
+
+/**
+ * update url link to save build information
+ */
+function updateURL() {
+
+}
+
+/**
+ * process the url parameters to load saved build
+ */
+function processURL() {
+
 }
